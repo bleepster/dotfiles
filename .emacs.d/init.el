e33cf644ebcfc207500efe6261cf5be549836e59
bleepster <earl.lapus@gmail.com>
2020-12-04 16:07:09 +0800

    add some comment to each section of the config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,53 +1,67 @@
+;; no startup message
 (setq inhibit-startup-message t)
+
+;; turn of bell sounds
 (setq visible-bell t)
 
+;; keep a minimalist window
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (tooltip-mode -1)
 (set-fringe-mode 10)
 (menu-bar-mode -1)
 
+;; inform emacs of where I am, this will be used by the theme-changer below
 (setq calendar-location-name "Cebu, PH")
 (setq calendar-latitude 10)
 (setq calendar-longitude 125)
 
+;; display line/column numbers everywhere
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; install/enable package and set where to retrieve them
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; install/enable use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; install/enable package: evil
 (use-package evil
   :ensure t
   :config
   (evil-mode 1))
 
+;; install/enable package: which-key
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
 
+;; install/enable package: gruvbox-theme 
 (use-package gruvbox-theme
   :ensure t)
 
+;; install/enable package: theme-changer
 (use-package theme-changer
   :ensure t
   :config
   (change-theme 'gruvbox-light-hard 'gruvbox-dark-soft))
 
-;; icons for doom-modeline
+;; install/enable package: all the icons, required by doom-modeline
 (use-package all-the-icons)
 
+;; install/enable package: doom-modeline
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
@@ -87,17 +101,21 @@
      (python . t))))
 
 
+;; install/enable package: org-bullets
 (use-package org-bullets
   :ensure t
   :hook (org-mode . org-bullets-mode)
   :config
   (setq org-bullet-list '("◉" "◎" "❖" "○" "►" "◇" "✸")))
 
+;; install/enable package: htmlize
 (use-package htmlize
   :ensure t)
 
+;; set Iosevka as the main font
 (set-frame-font "Iosevka Term Curly 12")
 
+;; enable ido mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
